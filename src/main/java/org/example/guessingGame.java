8f370120b79af160ca59d04b487eb47ac492a5b8
XixianWei <xixian.wei94@gmail.com>
2023-01-19 14:55:24 +0000

    added guessingGame

---
+++ src/main/java/org/example/guessingGame.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //set a secret number
+        int secretNumber = 7;
+        //Prompt user to input their favourite chocolate
+        System.out.println("What is the secret number?");
+        //collect your input
+        Scanner reader = new Scanner(System.in);
+        int guessingNumber = reader.nextInt();
+        // if the guessing number is the same to the secret number,print 'You're right!'
+        //if the guessing number is greater than the secret number, print 'Too high, try again.'
+        // if the guessing number is smaller than the secret number, print 'Too low, try again.'
+        if (guessingNumber == secretNumber){
+            System.out.println("You're right!");
+        } else if (guessingNumber > secretNumber) {
+            System.out.println("Too high, try again.");
+        }else {
+            System.out.println("Too low, try again.");
+        }
+
+    }
+}
